6a483eafec084e94686e84e3284c2f2bd1284e87
aymanTuet20123213 <46880832+aymanTuet20123213@users.noreply.github.com>
2019-01-28 12:32:40 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+/*
+Task 4 - Further learn about how fill and rect functions work by putting a rectangle exactly in the top left,
+        top right, bottom left and bottom right corners of the screen and also 1 rectangle in the middle.
+        Try to apply your algebraic knowledge of the canvas size to do this.
+*/
+
+
 function setup()
 {
   //create a drawing service 700px wide, 500px tall
@@ -15,11 +22,11 @@ function draw()
 
 
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
-  rect(10,10,20,99);
-  rect(150,150,175,175);
-
-
-
+  rect(0,0,10,20);
+  rect(489,0,10,20);
+  rect(250-50/2,250-50/2,50,50);
+  rect(489,479,10,20);
+  rect(0,479,10,20);
 
 
 
