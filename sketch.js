a816c2d27aa6fb1024c079538d89b6c7dcfbaacc
aymanTuet20123213 <46880832+aymanTuet20123213@users.noreply.github.com>
2019-02-18 12:42:55 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,27 @@
-/*
-Task 4 - Further learn about how fill and rect functions work by putting a rectangle exactly in the top left,
-        top right, bottom left and bottom right corners of the screen and also 1 rectangle in the middle.
-        Try to apply your algebraic knowledge of the canvas size to do this.
-*/
-
-
 function setup()
 {
-  //create a drawing service 700px wide, 500px tall
-  createCanvas(500,500);
+  createCanvas(700,500);
 
 }
 
 function draw()
 {
-  //create a background using RGB values
-  background(9,255,0)
+	background(83,222,217)
 
-  // change the paint brush to a specific color according to RGB values
-  fill(49,128,137);
+	fill(0,0,255);
+	stroke(0,0,0);
+	strokeWeight(10);
 
+	text("Hello World",150,30);
 
-  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
-  rect(0,0,10,20);
-  rect(489,0,10,20);
-  rect(250-50/2,250-50/2,50,50);
-  rect(489,479,10,20);
-  rect(0,479,10,20);
+	rect(10,10,100,50);
 
+	text("Ayman Tuet",30,50);
 
+	rect(10,100,100,50);
 
+	fill(112,108,174);
+
+	text("Donald Trump",30,130);
 
 }
